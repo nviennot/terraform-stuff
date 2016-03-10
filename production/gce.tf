d2cf348a6548e09bb891b5dd4b20029fbc0f160d
Nicolas Viennot <nicolas@viennot.biz>
2016-03-10 14:06:28 -0500

    wip

--- production/gce.tf
+++ production/gce.tf
@@ -40,6 +40,51 @@ module "gce_com_staging_workers" {
   image = "${atlas_artifact.travis-worker-gce-image.id}"
 }
 
+resource "google_compute_instance" "tmate-edge" {
+  count = "${var.instance_count}"
+  name = "travis-tmate-edge-gce-${var.site}-${var.environment}-${count.index + 1}"
+  machine_type = "${var.machine_type}"
+  zone = "${var.zone}"
+  tags = ["tmate-edge", "${var.environment}", "${var.site}"]
+
+  can_ip_forward = false
+
+  disk {
+    auto_delete = true
+    image = "${var.image}"
+    type = "pd-ssd"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = "${file(format("cloud-init/travis-tmate-edge-gce-%s-%s", var.site, var.environment))}"
+}
+
+module "gce_org_prod_tmate_edge" {
+  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
+  instance_count = 1
+  site = "org"
+  environment = "prod"
+  machine_type = "g1-small"
+  zone = "us-central1-b"
+  image = "${atlas_artifact.travis-tmate-edge-gce-image.id}"
+}
+
+module "gce_com_prod_tmate_edge" {
+  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
+  instance_count = 1
+  site = "com"
+  environment = "prod"
+  machine_type = "g1-small"
+  zone = "us-central1-b"
+  image = "${atlas_artifact.travis-tmate-edge-gce-image.id}"
+}
+
 resource "google_compute_instance" "ubuntu_trusty_micro_playground" {
   count = 1
   name = "ubuntu-trusty-micro-playground"
